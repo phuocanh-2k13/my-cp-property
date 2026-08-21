901a961bf21c4411be6fe25d31120e68d57a3e8f
Phuoc Anh <phuocanhha1742013@gmail.com>
2026-08-21 16:20:12 +0700

    .

--- LibraryChecker/deque/main.cpp
+++ LibraryChecker/deque/main.cpp
@@ -23,7 +23,30 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    deque<int> dque;
 
+    int q; cin >> q;
+    while (q--) {
+        int type; cin >> type;
+        if (type == 0) {
+            int x; cin >> x;
+            dque.push_front(x);
+        }
+        else if (type == 1) {
+            int x; cin >> x;
+            dque.push_back(x);
+        }
+        else if (type == 2) {
+            dque.pop_front();
+        }
+        else if (type == 3) {
+            dque.pop_back();
+        }
+        else {
+            int idx; cin >> idx;
+            cout << dque.at(idx) << '\n'; 
+        }
+    }
 
     return 0;
 }
